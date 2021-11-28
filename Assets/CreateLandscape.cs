31f1e4eddae199d146ac982652e4b209e05b471c
Ginele <gineletanis@gmail.com>
2021-11-28 17:26:42 -0500

    Grid Landscape + rotate turn
    Added code to generate city landscape and also code to make the plane rotate when turning/going up and down.

--- Assets/CreateLandscape.cs
+++ Assets/CreateLandscape.cs
@@ -6,19 +6,36 @@ public class CreateLandscape : MonoBehaviour
 {
 
     [Header("Plane")]
-    public GameObject planePrefab;
-    private Plane _plane;
+    
 
-    public int grid; 
+    public GameObject pF; 
+    public float dis;
+    public int gridX;
+    public int gridZ;
 
     
     void Awake()
     {
-        _plane = planePrefab.GetComponent<Plane>(); 
+        gridX = 10;
+        gridZ = 15;
+       // pF = pF.GetComponent<GameObject>();
+
+
+
+        for (int i = 0; i < gridX; i++)
+        {
+            for (int j = 0; j < gridZ; j++)
+            {
+                Vector3 position = new Vector3(i, 0, j) * dis;
+                Instantiate(pF, position, Quaternion.identity); 
+            }
+        }
+        
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         
     }
--- Assets/Scripts/MicController.cs
+++ Assets/Scripts/MicController.cs
@@ -74,7 +74,7 @@ public class MicController : MonoBehaviour
         int _sampleWindow = 128;
      
         //get data from microphone into audioclip
-        float  LevelMax()
+        public float  LevelMax()
         {
             float levelMax = 0;
             float[] waveData = new float[_sampleWindow];
@@ -99,7 +99,7 @@ public class MicController : MonoBehaviour
         {
             // levelMax equals to the highest normalized value power 2, a small number because < 1
             // pass the value to a static var so we can access it from anywhere
-            MicLoudness = LevelMax ();
+            //MicLoudness = LevelMax ();
             //Debug.Log(MicLoudness);
  
         }
